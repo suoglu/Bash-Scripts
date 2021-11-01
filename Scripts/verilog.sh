e88c06a0f79b30507fb7896503c384ec8d186a8a
suoglu <yigitsuoglu@icloud.com>
2021-11-01 15:23:32 +0100

    add source list for iverilog

--- Scripts/verilog.sh
+++ Scripts/verilog.sh
@@ -1,8 +1,62 @@
 #!/bin/bash
-# This script calls first icarus verilog, then vvp and removes .out
+# This script calls first checks for sources list, runs icarus verilog and vvp, removes .out and lunches GTKWave
 
-if [ $# -eq 1 ]; then
-  iverilog -Wall -o o.out $1
+SIM_DIR=""
+SRC_DIC=""
+SRC_FILE=""
+
+if [ $# -eq 2 ]; then
+  SRC_FILE=$2
+  echo "Using given source files list $2"
+else
+  if [ -d "Sim" ]; then
+    SIM_DIR="Sim/"
+  elif [ -d "sim" ]; then
+    SIM_DIR="sim/"
+  elif [ -d "Simulation" ]; then
+    SIM_DIR="Simulation/"
+  elif [ -d "simulation" ]; then
+    SIM_DIR="simulation/"
+  fi
+  if [ -d "src" ]; then
+    SRC_DIC="src/"
+  elif [ -d "Src" ]; then
+    SRC_DIC="Src/"
+  elif [ -d "Sources" ]; then
+    SRC_DIC="Sources/"
+  elif [ -d "sources" ]; then
+    SRC_DIC="sources/"
+  fi
+  if [ -f "$SIM_DIR""sources.txt" ]; then
+    SRC_FILE="$SIM_DIR""sources.txt"
+  elif [ -f "$SIM_DIR""Sources.txt" ]; then
+    SRC_FILE="$SIM_DIR""Sources.txt"
+  elif [ -f "$SIM_DIR""src.txt" ]; then
+    SRC_FILE="$SIM_DIR""src.txt"
+  elif [ -f "$SIM_DIR""Src.txt" ]; then
+    SRC_FILE="$SIM_DIR""Src.txt"
+  elif [ -f "$SRC_DIC""sources.txt" ]; then
+    SRC_FILE="$SRC_DIC""sources.txt"
+  elif [ -f "$SRC_DIC""Sources.txt" ]; then
+    SRC_FILE="$SRC_DIC""Sources.txt"
+  elif [ -f "$SRC_DIC""src.txt" ]; then
+    SRC_FILE="$SRC_DIC""src.txt"
+  elif [ -f "$SRC_DIC""Src.txt" ]; then
+    SRC_FILE="$SRC_DIC""Src.txt"
+  fi
+  
+  if [ $SRC_FILE == "" ]; then
+    echo "Cannot find source files list under names sources.txt or src.txt!"
+    echo "Searched sim/ simulation/ src/ sources/ and ./"
+    echo "Running without additional source files!"
+  else
+    echo "Found source file list $SRC_FILE"
+    SRC_FILE="-c "$SRC_FILE
+  fi
+fi
+
+if [ $# -eq 1 ] || [ $# -eq 2 ]; then
+  iverilog -Wall -o o.out $SRC_FILE $1
   if [ $? -ne 0 ]; then
     echo "iverilog...failed"
     echo "Exiting..."
@@ -20,10 +74,14 @@ if [ $# -eq 1 ]; then
       if [ $? -ne 0 ]; then
         echo "Cannot remove o.out!"
       else
-        echo "done"
+        echo "o.out removed"
       fi
+      echo "Running GTKWave"
+      gtkwave *.vcd
+      echo "Script done"
     fi
   fi
 else
-	echo "Usage: verilog.sh <testbench file>"
+	echo "Usage: verilog.sh <testbench file> [<Source file list>]"
 fi
+
